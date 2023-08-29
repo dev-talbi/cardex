2bf3e422536cbcd790be83704030f2e6a5efa04e
dev-talbi <dev.talbi@gmail.com>
2023-08-30 00:52:19 +0200

    in prg

---
+++ api/src/Entity/Card.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\CardRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CardRepository::class)]
+#[ApiResource]
+class Card
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 25)]
+    private ?string $tcgId = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTcgId(): ?string
+    {
+        return $this->tcgId;
+    }
+
+    public function setTcgId(string $tcgId): static
+    {
+        $this->tcgId = $tcgId;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
